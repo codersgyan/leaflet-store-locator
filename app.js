8887820169d487c705a7cfb4d78d7b1be4df7e09
Rakesh K <codersgyan@gmail.com>
2020-12-08 12:52:54 +0300

    initial

---
+++ app.js
@@ -0,0 +1,76 @@
+const myMap = L.map('map').setView([22.9074872, 79.07306671], 5);
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution =
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by coder\'s gyan with ❤️';
+const tileLayer = L.tileLayer(tileUrl, { attribution });
+tileLayer.addTo(myMap);
+
+
+function generateList() {
+  const ul = document.querySelector('.list');
+  storeList.forEach((shop) => {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const a = document.createElement('a');
+    const p = document.createElement('p');
+    a.addEventListener('click', () => {
+        flyToStore(shop);
+    });
+    div.classList.add('shop-item');
+    a.innerText = shop.properties.name;
+    a.href = '#';
+    p.innerText = shop.properties.address;
+
+    div.appendChild(a);
+    div.appendChild(p);
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
+}
+
+generateList();
+
+function makePopupContent(shop) {
+  return `
+    <div>
+        <h4>${shop.properties.name}</h4>
+        <p>${shop.properties.address}</p>
+        <div class="phone-number">
+            <a href="tel:${shop.properties.phone}">${shop.properties.phone}</a>
+        </div>
+    </div>
+  `;
+}
+function onEachFeature(feature, layer) {
+    layer.bindPopup(makePopupContent(feature), { closeButton: false, offset: L.point(0, -8) });
+}
+
+var myIcon = L.icon({
+    iconUrl: 'marker.png',
+    iconSize: [30, 40]
+});
+
+const shopsLayer = L.geoJSON(storeList, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+        return L.marker(latlng, { icon: myIcon });
+    }
+});
+shopsLayer.addTo(myMap);
+
+function flyToStore(store) {
+    const lat = store.geometry.coordinates[1];
+    const lng = store.geometry.coordinates[0];
+    myMap.flyTo([lat, lng], 14, {
+        duration: 3
+    });
+    setTimeout(() => {
+        L.popup({closeButton: false, offset: L.point(0, -8)})
+        .setLatLng([lat, lng])
+        .setContent(makePopupContent(store))
+        .openOn(myMap);
+    }, 3000);
+}
+
+
+
---
+++ stores.js
@@ -0,0 +1,350 @@
+const storeList = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.82952788802635, 18.920675417289807]
+        },
+        "properties": {
+          "name": "Pizza outlet 1",
+          "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.82815459698692, 18.94324557965778]
+        },
+        "properties": {
+          "name": "Pizza outlet 2",
+          "address": "Shop No 7, Ground Floor, Chemox House, Hospital Lane, Barrack Rd, New Marine Lines, Mumbai, Maharashtra 400020, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.81416419471911, 18.958588300624903]
+        },
+        "properties": {
+          "name": "Pizza outlet 3",
+          "address": "Mathew Rd, Charni Road East, Opera House, Gamdevi, Mumbai, Maharashtra 400004, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.82468278992283, 18.99953227821179]
+        },
+        "properties": {
+          "name": "Pizza outlet 4",
+          "address": "Unit NO. 6D, Block-17, Phoenix Mills Compound, 462, Senapati Bapat Marg, Lower Parel, Mumbai, Maharashtra 400013, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.87461342204949, 19.079330659542418]
+        },
+        "properties": {
+          "name": "Pizza outlet 5",
+          "address": "Shop No 2, Ground floor Kanakia Zillion, LBS Marg, Kurla West, Mumbai, Maharashtra 400070, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.87289714156712, 19.229326479731387]
+        },
+        "properties": {
+          "name": "Pizza outlet 6",
+          "address": "No. 1-5, Building, Shop, No. 35, Evershine Millennium Paradise, Thakur Village, Kandivali East, Mumbai, Maharashtra 400101, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.20889596191188, 28.68797557618975]
+        },
+        "properties": {
+          "name": "Pizza outlet 7",
+          "address": "Ground Floor,Shop No. A,Municipal No. 26A & 27A-UA Jawahar Nagar,Kamla Nagar, near Malka Ganj, Chowk, Delhi 110007, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.11585549399085, 28.741270652258336]
+        },
+        "properties": {
+          "name": "Pizza outlet 8",
+          "address": "SHOP NO. 10-11, COMMUNITY CENTRE, Garg Mall, Pocket 4, Sector 11, Rohini, New Delhi, Delhi 110020, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.30262307535264, 28.635858680378387]
+        },
+        "properties": {
+          "name": "Pizza outlet 9",
+          "address": "Plot No. 7,LSC, Pankaj Plaza, near Prince Apartments, I.P.Extension, Patparganj, New Delhi, Delhi 110092, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.27116736919079, 28.539914829877652]
+        },
+        "properties": {
+          "name": "Pizza outlet 10",
+          "address": "Plot No.16, Ground Floor, Community Center Rd, Pocket A, Okhla Phase I, Okhla Industrial Area, New Delhi, Delhi 110020, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [88.37799509128703, 22.632535906298756]
+        },
+        "properties": {
+          "name": "Pizza outlet 11",
+          "address": "Ground Floor, Shop no. 3 & 4, Dist, Chinar Park, Noapara, Sukanta Pally, Newtown, Kolkata, West Bengal 700157, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [88.35190256153795, 22.56692525562484]
+        },
+        "properties": {
+          "name": "Pizza outlet 12",
+          "address": "3A, Humayun Place Near New Empire Cinema, Esplanade, New Market Area, Dharmatala, Kolkata, West Bengal 700087, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [88.38795145132286, 22.70413303586267]
+        },
+        "properties": {
+          "name": "Pizza outlet 13",
+          "address": "Municipality, 352/1889, Dakshminayan Barasat Road Under Panhihati, Sodepur, Kolkata, West Bengal 700110, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [88.3251233867392, 22.520947492726815]
+        },
+        "properties": {
+          "name": "Pizza outlet 14",
+          "address": "Plot No. 99A, Block F, Nalini Ranjan Ave, New Alipore, Kolkata, West Bengal 700053, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.52382422967368, 23.080883179752515]
+        },
+        "properties": {
+          "name": "Pizza outlet 15",
+          "address": "Plot No. 50 & 51, Survey No. 301/2, 313/1, 304/2, Shop No. G - 7 & 8 Ground Floor, Opp. Gujarat High Court, S. G. Road Town Planning Scheme No. 28, Ahmedabad, Gujarat 380061, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.58939887680621, 23.055613560773505]
+        },
+        "properties": {
+          "name": "Pizza outlet 16",
+          "address": "Neelgagan Plaza, 1st And 2nd Floor Survey Number 2259 Commissioner Office, Dr Ambedkar Rd, Opposite Police, Jain Colony, Shahibag, Ahmedabad, Gujarat 380004, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.47507239777408, 23.033498752157918]
+        },
+        "properties": {
+          "name": "Pizza outlet 17",
+          "address": "A1 Amrapali Axiom, Sardar Patel Ring Rd, Ambli, Ahmedabad, Gujarat 380058, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.66939307985272, 23.05340224332604]
+        },
+        "properties": {
+          "name": "Pizza outlet 18",
+          "address": "Shop Number A/03 & A/04, Ground Floor, Gujarat 382350, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.87720724616587, 18.57728849915506]
+        },
+        "properties": {
+          "name": "Pizza outlet 19",
+          "address": "Shop no 31/A, Ground floor, Shoppers Orbit, Next Mahalaxsmi Complex Near Visharantwadi Police Chowki, Dhanori Rd, Pune, Maharashtra 411015, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.93213888774288, 18.495259471467133]
+        },
+        "properties": {
+          "name": "Pizza outlet 20",
+          "address": "Shop No:4,5,14,15,16 & 17 Fortune Plaza, Hadapsar, Pune, Maharashtra 411028, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.82364889562831, 18.48256098719139]
+        },
+        "properties": {
+          "name": "Pizza outlet 21",
+          "address": "Plot No. 7, Dhanlaxmi Society, Opp. Jagtap Hospital, Pune, Maharashtra 411051, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.84321829294011, 18.52228131664155]
+        },
+        "properties": {
+          "name": "Pizza outlet 22",
+          "address": "Bus Stop, Shop No. 6 & 7, opposite Deccan Gymkhana, Pune, Maharashtra 411004, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.79308003312877, 18.496413470622766]
+        },
+        "properties": {
+          "name": "Pizza outlet 23",
+          "address": "Cipla Foundation, Survey No. 116/1, Shop No. 2, Opposite, Warje, Pune, Maharashtra 411058, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [78.68705181099095, 10.799619385080312]
+        },
+        "properties": {
+          "name": "Pizza outlet 24",
+          "address": "Dukes Complex, 120, Bharathiar Salai, Melapudur, Cantonment, Tiruchirappalli, Tamil Nadu 620001, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [78.68405585878058, 10.834835813062087]
+        },
+        "properties": {
+          "name": "Pizza outlet 25",
+          "address": "Vignesh Plaza, A-6, Ground Floor, Thillai Nagar Main Rd, Tiruchirappalli, Tamil Nadu 620018, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [74.63322891528135, 26.582440343492785]
+        },
+        "properties": {
+          "name": "Pizza outlet 26",
+          "address": "Shop No. G5, G6 and G7, Ground Floor, Jaipur Rd, Hathi Bhata, Ajmer, Rajasthan 305001, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [75.12212052531665, 27.779609685150188]
+        },
+        "properties": {
+          "name": "Pizza outlet 27",
+          "address": "Domino'S Pizza Ground Floor Shop No 2 Shiv Mandir cinema, Fatehpur Rd, Sikar, Rajasthan 332001, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [76.77930464002718, 28.378539373887044]
+        },
+        "properties": {
+          "name": "Pizza outlet 28",
+          "address": "Khasra No. 489 & 490, V Square Mall, Alwar Bypass Road, Sector-15, Bhiwadi, Rajasthan 301019, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [75.85537784187994, 25.13798803924888]
+        },
+        "properties": {
+          "name": "Pizza outlet 29",
+          "address": "A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+          "phone": "23 2323 2323"
+        }
+      }
+]
